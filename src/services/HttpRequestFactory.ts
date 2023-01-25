87d669b01c226170e2c7157709813606513f56d8
Carla Maximo <carladmaximo@gmail.com>
2023-01-25 16:27:22 -0300

    httprequestfactory

---
+++ src/services/HttpRequestFactory.ts
@@ -0,0 +1,53 @@
+export class HttpRequestFactory {
+    request = require('request');
+
+    public getRequest(url: string) {
+        var options = {
+            'method': 'GET',
+            'url': url,
+            'headers': {}
+          };
+
+          this.request(options, function (error, response) {
+            this.exceptionValidade(response);
+            return response;
+          });
+    }    
+
+    public postRequest(url: string, body: string) {
+        let options = {
+            'method': 'POST',
+            'url': url,
+            'headers': {
+                'Content-Type': 'application/json'
+            }, 
+            body: body
+        };
+
+        this.request(options, function (error, response) {
+            this.exceptionValidade(response);
+            return response;
+        });
+    }
+
+    public deleteRequest(url: string) {
+        var options = {
+            'method': 'DELETE',
+            'url': url,
+            'headers': {
+            }
+          };
+
+          this.request(options, function (error, response) {
+            this.exceptionValidade(response);
+            return response;
+          });
+    }
+
+
+    private exceptionValidade(response): void{
+        if(response.statusCode() != 200) {
+            throw new Error(response.statusCode() + " - error: " + response.body());
+        }
+    }
+}
