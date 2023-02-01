0f89ab6bbc44722f1aefebd018d4dee7294fe8d8
Carla Maximo <carladmaximo@gmail.com>
2023-01-31 23:01:17 -0300

    fix docFromPdf

--- src/body/doc/DocFromPdf.ts
+++ src/body/doc/DocFromPdf.ts
@@ -1,10 +1,27 @@
+import { Signer } from "../signer/Signer";
 import { Doc } from "./Doc";
 
 export class DocFromPdf extends Doc {
     private url_pdf: string;
 
-    constructor(url_pdf: string) {
-        super(false, '', '', false, false, '', '', '', '', '', new Date(), false, [], [], 0);
+    constructor(
+        sandbox: boolean,
+        name: string,
+        lang: string,
+        disable_signer_emails: boolean, signed_file_only_finished: boolean,
+        brand_logo: string,
+        brand_primary_color: string,
+        brand_name: string,
+        external_id: string,
+        folder_path: string,
+        date_limit_to_sign: Date,
+        signature_order_active: boolean,
+        observers: string[],
+        signers: Signer[],
+        reminder_every_n_days: number,
+        url_pdf: string,
+        ) {
+        super(sandbox, name, lang, disable_signer_emails, signed_file_only_finished, brand_logo, brand_primary_color, brand_name, external_id, folder_path, date_limit_to_sign, signature_order_active, observers, signers, reminder_every_n_days);
         this.url_pdf = url_pdf;
     }
 
